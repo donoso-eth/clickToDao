78598d8cc5fc20da3f48b48ef557695c62ea5609
jdonosocoding <javier.donoso@nuilea.com>
2022-04-08 18:42:13 +0200

    anguilar-web3 package boiler plate

---
+++ src/app/6-super-fluid/super-fluid-demo.module.ts
@@ -0,0 +1,38 @@
+import { InjectionToken, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SuperFluidDemoComponent } from './super-fluid-demo/super-fluid-demo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+
+import { DialogModule, NotifierModule} from '../dapp-components'
+
+
+import { ICONTRACT_METADATA } from 'angular-web3';
+
+import SuperFluidMetadata from '../../assets/contracts/super_app_metadata.json';
+export const contractMetadata = new InjectionToken<ICONTRACT_METADATA>('contractMetadata')
+
+export const contractProvider= {provide: 'contractMetadata', useValue:SuperFluidMetadata };
+
+
+
+@NgModule({
+  declarations: [
+    SuperFluidDemoComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatButtonModule,
+    DialogModule,
+    NotifierModule
+  ],
+  exports: [
+    SuperFluidDemoComponent
+  ],
+  providers: [contractProvider]
+})
+export class SuperFluidDemoModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuperFluidDemoModule } from './6-super-fluid/super-fluid-demo.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DappInjectorModule } from './dapp-injector/dapp-injector.module';
+import { StoreModule } from '@ngrx/store';
+import { we3ReducerFunction } from 'angular-web3';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SuperFluidDemoModule,
+    BrowserAnimationsModule,
+    DappInjectorModule.forRoot({wallet:'burner', defaultNetwork:'localhost'}),
+    StoreModule.forRoot({web3: we3ReducerFunction}),
   ],
   providers: [],
   bootstrap: [AppComponent]
